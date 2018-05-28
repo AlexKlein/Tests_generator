9a415ecf6c60b4eae4664b5eb60231b04da05c62
Aleksandr Klein <mr.kleyn@mail.ru>
2018-05-28 23:36:56 +0500

    Update uat_check_dm_core.sql

--- Procedures/uat_check_dm_core.sql
+++ Procedures/uat_check_dm_core.sql
@@ -1,23 +1,23 @@
 begin
-    execute immediate('drop procedure uat_testuser.uat_check_dm_core');
+    execute immediate('drop procedure check_dm_dwh');
 exception
     when others then
         null;
 end;
 /
-create or replace procedure uat_testuser.uat_check_dm_core
+create or replace procedure check_dm_dwh
 /******************************* ALFA HISTORY *******************************************\
-Дата        Автор            ID       Описание
+Р”Р°С‚Р°        РђРІС‚РѕСЂ            ID       РћРїРёСЃР°РЅРёРµ
 ----------  ---------------  -------- ----------------------------------------------------
-25.04.2018  Клейн А.М.      [000000]  Создание процедуры генерации UAT для сравнения 
-                                      сущностей с ядром.
+25.04.2018  Klein A.M.      [000000]  РЎРѕР·РґР°РЅРёРµ РїСЂРѕС†РµРґСѓСЂС‹ РіРµРЅРµСЂР°С†РёРё UAT РґР»СЏ СЃСЂР°РІРЅРµРЅРёСЏ 
+                                      СЃСѓС‰РЅРѕСЃС‚РµР№ СЃ СЏРґСЂРѕРј.
 \******************************* ALFA HISTORY *******************************************/
 as
-    vOpDay varchar2(128);   -- параметр даты операционного дня
-    vSQL   varchar2(1024);  -- скрипт запроса
-    vCols  varchar2(1024);  -- текст выбора колонок
+    vOpDay varchar2(128);   -- РїР°СЂР°РјРµС‚СЂ РґР°С‚С‹ РѕРїРµСЂР°С†РёРѕРЅРЅРѕРіРѕ РґРЅСЏ
+    vSQL   varchar2(1024);  -- СЃРєСЂРёРїС‚ Р·Р°РїСЂРѕСЃР°
+    vCols  varchar2(1024);  -- С‚РµРєСЃС‚ РІС‹Р±РѕСЂР° РєРѕР»РѕРЅРѕРє
     
-    cursor cur_tab  -- курсор выборки нужных сущностей
+    cursor cur_tab  -- РєСѓСЂСЃРѕСЂ РІС‹Р±РѕСЂРєРё РЅСѓР¶РЅС‹С… СЃСѓС‰РЅРѕСЃС‚РµР№
     is
     select row_number ()
            over (partition by dummy
@@ -42,11 +42,7 @@ as
                            tab.table_name = col.table_name
            left outer join dual rownumb 
                         on 1 = 1
-           where  lower(tab.owner) = 'dwh' and
-                 (lower(tab.table_name) like '%dim'  or
-                  lower(tab.table_name) like '%hist' or
-                  lower(tab.table_name) like '%stat' or
-                  lower(tab.table_name) like '%tran')
+           where  lower(tab.owner) = 'dwh'
            group by tab.owner,
                     tab.table_name,
                     tab.owner||'.'||tab.table_name,
@@ -56,57 +52,57 @@ as
     order by type,
              table_name;
 
-    -- функция выбора параметра
+    -- С„СѓРЅРєС†РёСЏ РІС‹Р±РѕСЂР° РїР°СЂР°РјРµС‚СЂР°
     function f_find_param (p_param_name in varchar2) return varchar2
     is
-        -- перевод в режим автономной транзакции
+        -- РїРµСЂРµРІРѕРґ РІ СЂРµР¶РёРј Р°РІС‚РѕРЅРѕРјРЅРѕР№ С‚СЂР°РЅР·Р°РєС†РёРё
         pragma autonomous_transaction;
         
-        v_out varchar2 (255);  -- значение параметра
+        v_out varchar2 (255);  -- Р·РЅР°С‡РµРЅРёРµ РїР°СЂР°РјРµС‚СЂР°
     begin
         
-        -- выбор параметра
+        -- РІС‹Р±РѕСЂ РїР°СЂР°РјРµС‚СЂР°
         select param_value
         into   v_out
-        from   uat_global_param
+        from   t_params
         where  param_name = p_param_name;
 
-        -- возврат значения параметра
+        -- РІРѕР·РІСЂР°С‚ Р·РЅР°С‡РµРЅРёСЏ РїР°СЂР°РјРµС‚СЂР°
         return v_out;
     end f_find_param;
     
 begin
 
-    -- формирование даты операционного дня
-    --vOpDay := 'to_date('''||f_find_param('OPERATION_DAY')||''',''dd.mm.yyyy'')';
+    -- С„РѕСЂРјРёСЂРѕРІР°РЅРёРµ РґР°С‚С‹ РѕРїРµСЂР°С†РёРѕРЅРЅРѕРіРѕ РґРЅСЏ
+    -- vOpDay := 'to_date('''||f_find_param('OPERATION_DAY')||''',''dd.mm.yyyy'')';
     vOpDay := 'to_date(''*operation_day*'',''dd.mm.yyyy'')';
     
-    delete from uat_testuser.uat_test_analytical
+    delete from t_test
     where n_test > 300001;
     
     commit;
     
-    -- выполняется проход по сущностям выборки
+    -- РІС‹РїРѕР»РЅСЏРµС‚СЃСЏ РїСЂРѕС…РѕРґ РїРѕ СЃСѓС‰РЅРѕСЃС‚СЏРј РІС‹Р±РѕСЂРєРё
     for i in cur_tab loop
-        -- очистка списка параметров
+        -- РѕС‡РёСЃС‚РєР° СЃРїРёСЃРєР° РїР°СЂР°РјРµС‚СЂРѕРІ
         vCols := null;
         
-        -- разбор по типам сущностей
+        -- СЂР°Р·Р±РѕСЂ РїРѕ С‚РёРїР°Рј СЃСѓС‰РЅРѕСЃС‚РµР№
         if    lower(i.type) like '%dim' and
               lower(i.type) not like '%ldim' and
               lower(i.type) not like '%sdim' then
             
-            -- проверка на существование deleted_flag
+            -- РїСЂРѕРІРµСЂРєР° РЅР° СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ deleted_flag
             if lower(i.column_list) like '%deleted_flag%' then
                 vCols := vCols||chr(10)||'and deleted_flag = ''N''';
             end if;
             
-            -- проверка на существование validto
+            -- РїСЂРѕРІРµСЂРєР° РЅР° СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ validto
             if lower(i.column_list) like '%validto%' then
                 vCols := vCols||chr(10)||'and validto = date''5999-12-31''';
             end if;
             
-            -- сравнение количества строк на ядре и на РБ
+            -- СЃСЂР°РІРЅРµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° СЃС‚СЂРѕРє РЅР° СЏРґСЂРµ Рё РЅР° Р Р‘
             vSQL := 'select '||chr(10)||
                     '(select count(1) '||chr(10)|| 
                     'from   '||i.full_name ||chr(10)|| 
@@ -119,33 +115,33 @@ begin
                     '       '||vCols||') as kol '||chr(10)|| 
                     'from   dual';
             
-            -- вставка новых проверок
-            insert into uat_testuser.uat_test_analytical (project,
-                                                          owner,
-                                                          table_name,
-                                                          test_desc,
-                                                          emploees,
-                                                          n_test,
-                                                          sql_test_hash_value,
-                                                          test_sql_clob) values ('RBREG',
-                                                                                 'RBREG',
-                                                                                 i.table_name,
-                                                                                 i.full_name,
-                                                                                 'AUTO',
-                                                                                 300001+i.rownumber,
-                                                                                 null,
-                                                                                 to_clob(vSQL));
+            -- РІСЃС‚Р°РІРєР° РЅРѕРІС‹С… РїСЂРѕРІРµСЂРѕРє
+            insert into t_test (project,
+								owner,
+								table_name,
+								test_desc,
+								emploees,
+								n_test,
+								sql_test_hash_value,
+								test_sql_clob) values ('TST_ENV',
+													   'TST_ENV',
+													   i.table_name,
+													   i.full_name,
+													   'AUTO',
+													   300001+i.rownumber,
+													   null,
+													   to_clob(vSQL));
             commit;
    
         elsif lower(i.type) like '%ldim' or 
               lower(i.type) like '%sdim' then
             
-            -- проверка на существование deleted_flag
+            -- РїСЂРѕРІРµСЂРєР° РЅР° СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ deleted_flag
             if lower(i.column_list) like '%deleted_flag%' then
                 vCols := vCols||chr(10)||'and deleted_flag = ''N''';
             end if;
             
-            -- сравнение количества строк на ядре и на РБ
+            -- СЃСЂР°РІРЅРµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° СЃС‚СЂРѕРє РЅР° СЏРґСЂРµ Рё РЅР° Р Р‘
             vSQL := 'select '||chr(10)||
                     '(select count(1) '||chr(10)|| 
                     'from   '||i.full_name ||chr(10)|| 
@@ -158,42 +154,42 @@ begin
                     '       '||vCols||') as kol '||chr(10)|| 
                     'from   dual';
             
-            -- вставка новых проверок
-            insert into uat_testuser.uat_test_analytical (project,
-                                                          owner,
-                                                          table_name,
-                                                          test_desc,
-                                                          emploees,
-                                                          n_test,
-                                                          sql_test_hash_value,
-                                                          test_sql_clob) values ('RBREG',
-                                                                                 'RBREG',
-                                                                                 i.table_name,
-                                                                                 i.full_name,
-                                                                                 'AUTO',
-                                                                                 300001+i.rownumber,
-                                                                                 null,
-                                                                                 to_clob(vSQL));
+            -- РІСЃС‚Р°РІРєР° РЅРѕРІС‹С… РїСЂРѕРІРµСЂРѕРє
+            insert into t_test (project,
+                                owner,
+                                table_name,
+                                test_desc,
+                                emploees,
+                                n_test,
+                                sql_test_hash_value,
+                                test_sql_clob) values ('TST_ENV',
+                                                       'TST_ENV',
+                                                       i.table_name,
+                                                       i.full_name,
+                                                       'AUTO',
+                                                       300001+i.rownumber,
+                                                       null,
+                                                       to_clob(vSQL));
             commit;
         
         elsif lower(i.type) like '%hist' then
 
-            -- проверка на существование deleted_flag
+            -- РїСЂРѕРІРµСЂРєР° РЅР° СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ deleted_flag
             if lower(i.column_list) like '%deleted_flag%' then
                 vCols := vCols||chr(10)||'and deleted_flag = ''N''';
             end if;
             
-            -- проверка на существование validto
+            -- РїСЂРѕРІРµСЂРєР° РЅР° СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ validto
             if lower(i.column_list) like '%validto%' then
                 vCols := vCols||chr(10)||'and validto = date''5999-12-31''';
             end if;
             
-            -- проверка на существование effective_to
+            -- РїСЂРѕРІРµСЂРєР° РЅР° СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ effective_to
             if lower(i.column_list) like '%effective_to%' then
                 vCols := vCols||chr(10)||'and effective_to >= '||vOpDay||'-100';
             end if;
 
-            -- сравнение количества строк на ядре и на РБ
+            -- СЃСЂР°РІРЅРµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° СЃС‚СЂРѕРє РЅР° СЏРґСЂРµ Рё РЅР° Р Р‘
             vSQL := 'select '||chr(10)||
                     '(select count(1) '||chr(10)|| 
                     'from   '||i.full_name ||chr(10)|| 
@@ -206,43 +202,43 @@ begin
                     '       '||vCols||') as kol '||chr(10)|| 
                     'from   dual';
             
-            -- вставка новых проверок
-            insert into uat_testuser.uat_test_analytical (project,
-                                                          owner,
-                                                          table_name,
-                                                          test_desc,
-                                                          emploees,
-                                                          n_test,
-                                                          sql_test_hash_value,
-                                                          test_sql_clob) values ('RBREG',
-                                                                                 'RBREG',
-                                                                                 i.table_name,
-                                                                                 i.full_name,
-                                                                                 'AUTO',
-                                                                                 300001+i.rownumber,
-                                                                                 null,
-                                                                                 to_clob(vSQL));
+            -- РІСЃС‚Р°РІРєР° РЅРѕРІС‹С… РїСЂРѕРІРµСЂРѕРє
+            insert into t_test (project,
+                                owner,
+                                table_name,
+                                test_desc,
+                                emploees,
+                                n_test,
+                                sql_test_hash_value,
+                                test_sql_clob) values ('TST_ENV',
+                                                       'TST_ENV',
+                                                       i.table_name,
+                                                       i.full_name,
+                                                       'AUTO',
+                                                       300001+i.rownumber,
+                                                       null,
+                                                       to_clob(vSQL));
             commit;
         
         elsif lower(i.type) like '%stat' or 
               lower(i.type) like '%tran' then
             
-            -- проверка на существование deleted_flag
+            -- РїСЂРѕРІРµСЂРєР° РЅР° СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ deleted_flag
             if lower(i.column_list) like '%deleted_flag%' then
                 vCols := vCols||chr(10)||'and deleted_flag = ''N''';
             end if;
             
-            -- проверка на существование validto
+            -- РїСЂРѕРІРµСЂРєР° РЅР° СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ validto
             if lower(i.column_list) like '%validto%' then
                 vCols := vCols||chr(10)||'and validto = date''5999-12-31''';
             end if;
             
-            -- проверка на существование value_day
+            -- РїСЂРѕРІРµСЂРєР° РЅР° СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ value_day
             if lower(i.column_list) like '%value_day%' then
                 vCols := vCols||chr(10)||'and value_day >= '||vOpDay||'-30';
             end if;
             
-            -- сравнение количества строк на ядре и на РБ
+            -- СЃСЂР°РІРЅРµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° СЃС‚СЂРѕРє РЅР° СЏРґСЂРµ Рё РЅР° Р Р‘
             vSQL := 'select '||chr(10)||
                     '(select count(1) '||chr(10)|| 
                     'from   '||i.full_name ||chr(10)|| 
@@ -255,22 +251,22 @@ begin
                     '       '||vCols||') as kol '||chr(10)|| 
                     'from   dual';
             
-            -- вставка новых проверок
-            insert into uat_testuser.uat_test_analytical (project,
-                                                          owner,
-                                                          table_name,
-                                                          test_desc,
-                                                          emploees,
-                                                          n_test,
-                                                          sql_test_hash_value,
-                                                          test_sql_clob) values ('RBREG',
-                                                                                 'RBREG',
-                                                                                 i.table_name,
-                                                                                 i.full_name,
-                                                                                 'AUTO',
-                                                                                 300001+i.rownumber,
-                                                                                 null,
-                                                                                 to_clob(vSQL));
+            -- РІСЃС‚Р°РІРєР° РЅРѕРІС‹С… РїСЂРѕРІРµСЂРѕРє
+            insert into t_test (project,
+                                owner,
+                                table_name,
+                                test_desc,
+                                emploees,
+                                n_test,
+                                sql_test_hash_value,
+                                test_sql_clob) values ('TST_ENV',
+                                                       'TST_ENV',
+                                                       i.table_name,
+                                                       i.full_name,
+                                                       'AUTO',
+                                                       300001+i.rownumber,
+                                                       null,
+                                                       to_clob(vSQL));
             commit;
             
         end if;
